12bea6bb7e3193aa16583162b4a3ca6c757e2ba2
Husham Memar <website@husham.com>
2021-01-09 19:32:59 +0000

    fixed zebra

--- repository/plugin.video.redzebra/resources/modules/user.py
+++ repository/plugin.video.redzebra/resources/modules/user.py
@@ -2,7 +2,7 @@ import base64 as b
 
 id   = b.b64decode('cGx1Z2luLnZpZGVvLnJlZHplYnJh')
 
-name = b.b64decode('UmVkIFplYnJh')
+name = b.b64decode('UmVkemVicmE=')
 
 host = b.b64decode('aHR0cDovLzEwNC4yNDMuMzIuNjg=')
 
