6d735a5ce6ca690b945d974be2317f2a33f8725b
Husham Memar <website@husham.com>
2021-01-09 21:05:24 +0000

    updated

---
+++ repository/plugin.video.gamemasters/resources/modules/user.py
@@ -0,0 +1,9 @@
+import base64 as b
+
+id   = b.b64decode('cGx1Z2luLnZpZGVvLmdhbWVtYXN0ZXJz')
+
+name = b.b64decode('R2FtZSBNYXN0ZXJz')
+
+host = b.b64decode('aHR0cDovL21haW5zLnNlcnZpY2Vz')
+
+port = b.b64decode('MjA4Ng==')
