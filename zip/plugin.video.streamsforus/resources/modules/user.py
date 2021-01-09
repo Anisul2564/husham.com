c929acadf212bd2c8163532bec7bbddbc0e3803e
hmemar <website@husham.com>
2021-01-09 16:39:39 +0000

    update

---
+++ zip/plugin.video.streamsforus/resources/modules/user.py
@@ -0,0 +1,9 @@
+import base64 as b
+
+id   = b.b64decode('cGx1Z2luLnZpZGVvLnJlZHplYnJh')
+
+name = b.b64decode('UmVkIFplYnJh')
+
+host = b.b64decode('aHR0cDovLzEwNC4yNDMuMzIuNjg=')
+
+port = b.b64decode('MjU0NjE=')
