76d0cfa0d670bbb05942353c504e06c5a29169e5
Husham Memar <website@husham.com>
2021-01-13 18:24:59 +0000

    update

--- repository2/plugin.video.redzebra/resources/modules/user.py
+++
@@ -1,9 +0,0 @@
-import base64 as b
-
-id   = b.b64decode('cGx1Z2luLnZpZGVvLnJlZHplYnJh')
-
-name = b.b64decode('UmVkemVicmE=')
-
-host = b.b64decode('aHR0cDovLzEwNC4yNDMuMzIuNjg=')
-
-port = b.b64decode('MjU0NjE=')
